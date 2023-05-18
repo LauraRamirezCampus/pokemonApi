d225c429609229dc24aa6b89782cfd9c2fed7e62
LauraRamirezCampus <lauravramirez0327@gmail.com>
2023-05-18 16:28:40 -0500

    feat: :sparkles: modificacion del buscador
    Modifica la funcion buscar para buscar un pokemon desde un input por nombre o por id

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
- function buscar(){
-     fetch(`https://pokeapi.co/api/v2/pokemon`)
-     .then(res=>res.json())
-     .then(data=>{
-         console.log(data,"p")});
-         let nombre= data.name;
 
- }
+function buscar(){
+    let busca= document.getElementById("buscando").value;
+    console.log(busca,"buscando");
+    fetch(`https://pokeapi.co/api/v2/pokemon/${busca}`)
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+                 document.getElementById("poki").innerHTML=`
+                 <div class="col">
+                 <article id="general">
+                 <div class="image-container">
+                 <h2>${data.name}</h2>
+                 <img src="${data.sprites.front_default}" id="img2" alt="">
+                 </div>
+                 <h3></h3>
+             <span></span>
+         </article>
+         </div>`
+    })
+}
+
+   
+    
+
  function pokemon(){
      for(let i=0; i< 11; i++){
             fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then(res=>res.json())
             .then(data=>{
-                console.log(data);
+                //console.log(data);
                 document.getElementById("poke").innerHTML+=`
                 <div class="col">
                 <article id="general">
                 <div class="image-container">
                 <h2>${data.name}</h2>
-                <img src="${data.sprites.front_default}" alt="">
+                <img src="${data.sprites.front_default}" class="imagen" alt="">
                 </div>
                 <h3></h3>
             <span></span>
@@ -28,74 +45,61 @@
 }
 pokemon()
 
-function obtnerPokemon1(){
-    let id1=Math.floor(Math.random()*1200+1);
-    let id2=Math.floor(Math.random()*1200+1);
-    let p1 , p2;
-    const url="https://pokeapi.co/api/v2/pokemon/1";
-    fetch(`${url}${id1}`)
-    .then(resp => resp.json())
-    .then(datos1 =>{
-        //console.log(datos1);
-        let nombre= datos1.name;
-        let img=datos1.sprites.front_default;
-        //console.log(nombre);
-        //console.log(img);
-        document.getElementById("pk").innerHTML=`
-        <article id="batalla">
-        <div class="image-container">
-        <h2>${nombre}</h2>
-        <img src="${img}" alt="">
-        </div>
-        <h3></h3>
-        <span></span>
-        </article>`
-        p1 = datos1;
-        console.log(p1);
-    })
-    fetch(`${url}${id2}`)
-    .then(resp => resp.json())
-    .then(datos2 =>{
-        //console.log(datos2);
-        let nombre= datos2.name;
-        let img=datos2.sprites.back_default;
-        //console.log(img);
-        //  console.log(nombre);
-        document.getElementById("pk2").innerHTML=`
-        <article id="batalla">
-        <div class="image-container">
-        <h2>${nombre}</h2>
-        <img src="${img}" alt="">
-        </div>
-        <h3></h3>
-        <span></span>
-        </article>`
-        p2 = datos2;
-        
-        if (p1.base_experience > p2.base_experience) {
-            console.log(p1.name + "es el ganador ");
-            console.log(p1);
-            
-        }else {
-            console.log(p2.name + "es el ganador ");
-            console.log(p2);
-            
-        }
-    })
-    
-    
-}
-
-let busca= document.getElementById("buscando")
- busca.addEventListener("input",(e)=>{
-    
-    buscar()
-     	console.log(busca.value,"busca");
+ function obtnerPokemon1(){
+     let id1=Math.floor(Math.random()*100+1);
+     let id2=Math.floor(Math.random()*100+1);
+     let p1 , p2;
+     const url="https://pokeapi.co/api/v2/pokemon/1";
+     fetch(`${url}${id1}`)
+     .then(resp => resp.json())
+     .then(datos1 =>{
+         //console.log(datos1);
+         let nombre= datos1.name;
+         let img=datos1.sprites.front_default;
+         //console.log(nombre);
+         //console.log(img);
+         document.getElementById("pk").innerHTML=`
+         <article id="batalla">
+         <div class="image-container">
+         <h2>${nombre}</h2>
+         <img src="${img}" alt="">
+         </div>
+         <h3></h3>
+         <span></span>
+         </article>
+         <div class="col"><img src="https://static.vecteezy.com/system/resources/thumbnails/011/020/248/small/versus-illustration-with-fire-sparks-or-smoke-fire-explosions-and-letters-vs-versus-background-png.png" alt=""></div>
+         `
+         p1 = datos1;
+         console.log(p1);
      })
+     fetch(`${url}${id2}`)
+     .then(resp => resp.json())
+     .then(datos2 =>{
+         //console.log(datos2);
+         let nombre= datos2.name;
+         let img=datos2.sprites.back_default;
+         //console.log(img);
+         //  console.log(nombre);
+         document.getElementById("pk2").innerHTML=`
+         <article id="batalla">
+         <div class="image-container">
+         <h2>${nombre}</h2>
+         <img src="${img}" alt="">
+         </div>
+         <h3></h3>
+         <span></span>
+         </article>`
+         p2 = datos2;      
+         if (p1.base_experience > p2.base_experience) {
+             console.log(p1.name + "es el ganador ");
+             console.log(p1);          
+         }else {
+             console.log(p2.name + "es el ganador ");
+             console.log(p2);          
+         }
+     })  
+    
+    }
+obtnerPokemon1()
 
-
-
-
-
-    obtnerPokemon1()
     
